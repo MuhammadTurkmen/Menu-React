c5708e0eab54f1f73fcfc95823692669301977e8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 10:45:43 +0430

    init

--- src/Menu.js
+++ src/Menu.js
@@ -5,6 +5,7 @@ const Menu = ({items}) => {
   return <div className='section-center'>
     {items.map((menuItem) => {
       const {id, title,img, desc} = menuItem
+      return <article key={id}></article>
     })}
   </div>
 };
