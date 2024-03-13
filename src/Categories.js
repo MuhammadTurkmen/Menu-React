96cab9a0b65f768a31fb91482f4493b824a37a07
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 11:21:28 +0430

    init

--- src/Categories.js
+++ src/Categories.js
@@ -4,7 +4,8 @@ const Categories = ({categories, filterItems}) => {
 
   return <div className="btn-container">
     {categories.map((category, index) => {
-      return <button className='filter-btn' type='button' key={index} onClick={category}>
+      return <button className='filter-btn' type='button' key={index} 
+      onClick={() => filterItems(category)}>
         {category}
       </button>
     })}
