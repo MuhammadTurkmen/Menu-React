3e50d0b35c655cd40512f9aa95f88f43c2f92065
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 11:20:54 +0430

    init

--- src/Categories.js
+++ src/Categories.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 const Categories = ({categories, filterItems}) => {
 
-  return <div className='btn-container'>
+  return <div className="btn-container">
     {categories.map((category, index) => {
-      return <button type='button' className='filter-btn' key={index}
-      onClick={() => filterItems(category)}>
+      return <button className='filter-btn' type='button' key={index} onClick={category}>
         {category}
       </button>
     })}
