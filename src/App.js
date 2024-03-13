1bcc6a22aa44398f44c86a2320d5795c2ae4efae
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 10:39:26 +0430

    init

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
-
+ 
 function App() {
+  const [menuItems, setMenuItems] = useState(items)
   return <h2>menu project setup</h2>;
 }
 
