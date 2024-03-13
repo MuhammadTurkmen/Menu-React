98643d7a03520a341e55486d740e00cad3c89f0d
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 11:04:43 +0430

    init

--- src/Categories.js
+++ src/Categories.js
@@ -4,7 +4,7 @@ const Categories = ({filterItems}) => {
 
   return <div className='btn-container'>
     <button className='filter-btn' onClick={() => filterItems('breakfast')}>
-      
+      breakfast
     </button>
   </div>
 }; 
