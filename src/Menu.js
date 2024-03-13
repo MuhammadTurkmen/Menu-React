b03f802a1b9be76c18ef232242db28f5f750151e
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 10:47:03 +0430

    init

--- src/Menu.js
+++ src/Menu.js
@@ -7,6 +7,12 @@ const Menu = ({items}) => {
       const {id, title,img, desc} = menuItem
       return <article key={id} className='menu-item'>
         <img src={img} alt={title} className='photo' />
+        <div className="item-info">
+          <header>
+            <h4>{title}</h4>
+            
+          </header>
+        </div>
       </article>
     })}
   </div>
