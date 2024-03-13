8f5f3227d8021dffdbcd1ba4548719b24916eab4
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 10:45:04 +0430

    init

--- src/Menu.js
+++ src/Menu.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Menu = ({items}) => {
    
   return <div className='section-center'>
-    
+    {items.map((menuItem) => {
+      const {id, }
+    })}
   </div>
 };
 
