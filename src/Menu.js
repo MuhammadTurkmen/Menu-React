6a1aa3b56ca088e448abccb3d05d5206da2a13ae
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 10:53:23 +0430

    init

--- src/Menu.js
+++ src/Menu.js
@@ -1,21 +1,18 @@
 import React from 'react';
 
 const Menu = ({items}) => {
-   
-  return <div className='section-center'>
-    {items.map((menuItem) => {
-      const {id, title,img, desc, price} = menuItem
-      return <article key={id} className='menu-item'>
-        <img src={img} alt={title} className='photo' />
-        <div className="item-info">
-          <header>
-            <h4>{title}</h4>
-            <h4 className="price">{price}</h4>
-          </header>
-        </div>
-      </article>
-    })}
-  </div>
+  return (
+    <div className="section-center">
+      {items.map((menuItem) => {
+        const {id, title, img, desc, price} = menuItem
+        return (
+          <article key={id} className='menu-item'>
+            <img src={img} alt={title} className='photo' />
+          </article>
+        )
+      })}
+    </div>
+  )
 };
 
 export default Menu;
