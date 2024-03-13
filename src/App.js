99c56adc39cfddf1320ef52361d2bc61a2850f6b
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 11:07:14 +0430

    init

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   const filterItems = (category) => {
     if(category === 'all') {
       setMenuItems(items)
+      return
     }
     const newItems = items.filter((item) => item.category === category)
     setMenuItems(newItems)
