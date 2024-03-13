452c50f25545742ebbcd900d63000db536b16d78
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 11:03:10 +0430

    init

--- src/Categories.js
+++ src/Categories.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Categories = ({filterItems}) => {
 
   return <div className='btn-container'>
-    <button></button>
+    <button className='filter-btn'></button>
   </div>
 }; 
 
