e6c5b470cafdc7300f89b15aa3bc129738429f96
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 11:17:00 +0430

    init

--- src/Categories.js
+++ src/Categories.js
@@ -3,12 +3,9 @@ import React from 'react';
 const Categories = ({categories, filterItems}) => {
 
   return <div className='btn-container'>
-    <button className='filter-btn' onClick={() => filterItems('all')}>
-      all
-    </button>
-    <button className='filter-btn' onClick={() => filterItems('breakfast')}>
-      breakfast
-    </button>
+    {categories.map((category) => {
+      
+    })}
   </div>
 }; 
 
